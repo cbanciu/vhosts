81a8df16a10bd2a44528b446071dd47b2a573830
Cristian Banciu <cbanciu@gmail.com>
2015-11-09 17:19:45 +0000

    Bug fixes

--- vhosts.py
+++ vhosts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Application to nicely display informations about Apache Virtual Hosts"""
 
-import platform
+import subprocess
 from glob import glob
 import itertools
 import os
@@ -24,29 +24,32 @@ class bcolors:
     CYAN = '\033[96m'
     UNDERLINE = '\033[4m'
 
-def get_distro():
-    """Name of your Linux distro"""
-    return platform.dist()[0].lower()
-
 
 def apache_files():
-    if get_distro() in ("centos", "redhat"):
-        webserver = "httpd"
-        apache_conf = "/etc/httpd/conf/httpd.conf"
-        apache_root = "/etc/httpd/"
-    elif get_distro() in ("ubuntu", "debian"):
-        webserver = "apache2"
-        apache_conf = "/etc/apache2/apache2.conf"
-        apache_root = "/etc/apache2/"
+    """Get Apache HTTPD_ROOT and SERVER_CONFIG_FILE"""
+
+    if subprocess.call("type " + "apache2ctl", shell=True, stdout=open('/dev/null', 'w'), stderr=open('/dev/null', 'w')) == 0:
+        command = 'apache2ctl -V 2>/dev/null | egrep "SERVER_CONFIG_FILE|HTTPD_ROOT" | cut -d= -f2'
+    elif subprocess.call("type " + "apachectl", shell=True, stdout=subprocess.PIPE, stderr=open('/dev/null', 'w')) == 0:
+        command = 'apachectl -V 2>/dev/null | egrep "SERVER_CONFIG_FILE|HTTPD_ROOT" | cut -d= -f2'
     else:
-        print bcolors.FAIL + "This application only works on RedHat, Ubuntu or Debian" + bcolors.ENDC
+        print bcolors.FAIL + "Cannot open Apache config file. Is Apache installed?" + bcolors.ENDC
         exit(1)
-    return webserver, apache_conf, apache_root
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    apache_files = proc.stdout.read().split()
+    for i in range(2):
+        if apache_files[i][0] in ('"', "'") and apache_files[i][0] == apache_files[i][-1]:
+            apache_files[i] = apache_files[i][1:-1]
+    if apache_files[1][0] is not "/":
+        apache_files[1] = apache_files[0]+"/"+apache_files[1]
+    return apache_files
+
 
 def get_conf_files(f):
     """Get a list of Apache config files starting from httpd.conf or apache2.conf"""
+
     apache_conf = apache_files()[1]
-    apache_root = apache_files()[2]
+    apache_root = apache_files()[0]+"/"
     conf_files = [[apache_conf]]
     try:
         fi = open(f, "r")
@@ -67,20 +70,22 @@ def get_conf_files(f):
                 file_to_add = glob(line_to_add)
                 conf_files.append(file_to_add)
             for files in file_to_add:
-               get_conf_files(files)
+                get_conf_files(files)
     fi.close()
     return list(set(itertools.chain(*conf_files)))
 
-def get_line(ln,st):
+
+def get_line(ln, st):
     if ln.lower().strip().startswith(st):
         if st.lower().strip() == "<virtualhost":
-            return re.search(r'\:(.*)\>',ln.strip(":").split()[1]).group(1) or "80"
+            return re.search(r'\:(.*)\>', ln.strip(":").split()[1]).group(1) or "80"
         if st.lower().strip() == "serveralias":
-            return re.sub(r'^(\w+ )',r'', ln.strip())
+            return re.sub(r'^(\w+ )', r'', ln.strip())
         else:
-            return ln.strip().split()[1] 
+            return ln.strip().split()[1]
     else:
         return "-"
+
 
 def get_vhosts():
     """Iterate through vhost list and get everything between <VirtualHost> and </VirtualHost>"""
@@ -103,14 +108,16 @@ def get_vhosts():
                 vhost.append(line)
     return all_vhosts
 
-def test_string(vhost_list,text):
+
+def test_string(vhost_list, text):
     s = vhost_list.splitlines()
     for i in s:
-        if get_line(i,text) is "-":
-            get_line(i,text)
-        else: 
-            return  get_line(i,text)
-    return get_line(i,text)
+        if get_line(i, text) is "-":
+            get_line(i, text)
+        else:
+            return  get_line(i, text)
+    return get_line(i, text)
+
 
 def get_vhost_dict():
     """Parse vhost list and generates the following dictionary:
@@ -119,26 +126,27 @@ def get_vhost_dict():
     sites = get_vhosts()
     vhost_dict = {}
     for site in sites:
-        if test_string(site,"servername") is not "-": #!= "-":
-            vhost_dict[test_string(site,"servername")+":"+test_string(site,"<virtualhost")] = [test_string(site,"documentroot")] + \
-            [test_string(site,"customlog")] + \
-            [test_string(site,"errorlog")] + \
-            [test_string(site,"configfile")]
-        if test_string(site,"serveralias") is not "-":
-            for s_alias in test_string(site,"serveralias").split():
-                vhost_dict[s_alias+":"+test_string(site,"<virtualhost")] = [test_string(site,"documentroot")] + \
+        if test_string(site, "servername") is not "-":
+            vhost_dict[test_string(site, "servername") + ":" + test_string(site, "<virtualhost")] = [test_string(site, "documentroot")] + \
+            [test_string(site, "customlog")] + \
+            [test_string(site, "errorlog")] + \
+            [test_string(site, "configfile")]
+        if test_string(site, "serveralias") is not "-":
+            for s_alias in test_string(site, "serveralias").split():
+                vhost_dict[s_alias + ":" + test_string(site, "<virtualhost")] = [test_string(site, "documentroot")] + \
                 [test_string(site,"customlog")] + \
                 [test_string(site,"errorlog")] + \
-                [test_string(site,"configfile")]
+                [test_string(site, "configfile")]
     return vhost_dict
 
 
 def print_header():
     text_header = "DOCUMENTROOT ACCESS_LOG ERROR_LOG CONFIG_FILE"
     try:
-        print bcolors.HEADER + "{:30}".format("SERVERNAME") + "" .join("{:35}".format(k) for k in text_header.split()) + bcolors.ENDC
+        print bcolors.HEADER + "{:30}".format("SERVERNAME") + "" .join("{:45}".format(k) for k in text_header.split()) + bcolors.ENDC
     except:
-        print bcolors.HEADER + "SERVERNAME".ljust(30) + "".join(k.ljust(35) for k in text_header.split()) + bcolors.ENDC
+        print bcolors.HEADER + "SERVERNAME".ljust(30) + "".join(k.ljust(45) for k in text_header.split()) + bcolors.ENDC
+
 
 def list_vhost(vhost):
     vhost_dict = get_vhost_dict()
@@ -146,36 +154,27 @@ def list_vhost(vhost):
     if keys:
         print_header()
         for key in keys:
-            try: #print i," ".join(vhost_dict[i])
-                print bcolors.OKBLUE + '{:30}'.format(key) + bcolors.ENDC + "".join("{:35}".format(v) for v in vhost_dict[key])
+            try:
+                print bcolors.OKBLUE + '{:30}'.format(key) + bcolors.ENDC + "".join("{:45}".format(v) for v in vhost_dict[key])
             except:
-                print bcolors.OKBLUE + key.ljust(30) + bcolors.ENDC + "".join(value.ljust(35) for value in vhost_dict[key])
+                print bcolors.OKBLUE + key.ljust(30) + bcolors.ENDC + "".join(value.ljust(45) for value in vhost_dict[key])
     else:
-        print "ServerName or ServerAlias " + bcolors.BOLD + vhost + bcolors.ENDC + \
-        " doesn't seem to be defined in your Apache configuration"
+        print "ServerName or ServerAlias " + bcolors.FAIL + vhost + bcolors.ENDC + \
+            " doesn't seem to be defined in your Apache configuration"
 
 
 def list_all():
     vhost_dict = get_vhost_dict()
     print_header()
-    for value,key  in sorted([(value,key) for (key,value) in vhost_dict.items()]):
+    for value, key in sorted([(value, key) for (key, value) in vhost_dict.items()]):
         try:
-            print bcolors.OKBLUE + '{:30}'.format(key) + " " + bcolors.ENDC + "".join('{:35}'.format(v) for v in value)
+            print bcolors.OKBLUE + '{:30}'.format(key) + " " + bcolors.ENDC + "".join('{:45}'.format(v) for v in value)
         except:
-            print bcolors.OKBLUE + key.ljust(30) + bcolors.ENDC + "".join(v.ljust(35) for v in value)
+            print bcolors.OKBLUE + key.ljust(30) + bcolors.ENDC + "".join(v.ljust(45) for v in value)
+
 
 def main():
-    #parser = ArgumentParser(description="Nicely display Apache virtual hosts info")
-    #parser.add_argument("-d", "--domain", help="Display domain(s) virtualhost info", nargs="*", type=str)
-    #args = parser.parse_args()
-    #if args.domain:
-    #    print_header()
-    #    for domain in args.domain:
-    #        list_vhost(domain)
-    #else:
-    #    list_all()
-    
-    parser = OptionParser(usage="usage: %prog [options]", version="%prog 0.6-beta", add_help_option=True) 
+    parser = OptionParser(usage="usage: %prog [options]", version="%prog 0.6-beta", add_help_option=True)
     parser.add_option("-d", "--domain", help="Display domain virtualhost info", dest="domain", action="store")
     (options, args) = parser.parse_args()
 
